1bd6062156bac6c2d494bcadc814cb94536b5205
luckybxl <luccia.g@gmail.com>
2018-04-27 15:57:34 +0200

    creation config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Constantes de connexion à notre DB
+ */
+define("DB_HOST","localhost");
+define("DB_PORT",3306);
+define("DB_NAME","tchat18cf2m");
+define("DB_LOGIN","root");
+define("DB_PWD","");
+define("DB_CHARSET","utf8");
