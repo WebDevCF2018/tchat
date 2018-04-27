2eee59ec1760758264c74291029b04fe29d746c2
Ichtuus <nicolas.diarra@outlook.fr>
2018-04-27 16:29:54 +0200

    actualisation des constantes

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$mysqli = @mysqli_connect('localhost','root','','tchat18cf2m',3306);
+$mysqli = @mysqli_connect('DB_HOST','DB_LOGIN','DB_PWD','DB_NAME',DB_PORT);
 
 if(mysqli_connect_error($mysqli)){
     die("Numéro d'erreur: ".mysqli_connect_errno($mysqli)."<br>Affichage de l'erreur: ".utf8_encode(mysqli_connect_error($mysqli)));
 }
 
-mysqli_set_charset($mysqli,'utf8');
+mysqli_set_charset($mysqli,'DB_CHARSET');
