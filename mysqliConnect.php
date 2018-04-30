4c8c43192a473811c6517017839b3ba735a056a6
Dimitri Moyson <dimitri.moyson@cf2m.lan>
2018-04-30 09:48:47 +0200

    correction mysqliConnect 0.1

--- mysqliConnect.php
+++ mysqliConnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$mysqli = @mysqli_connect('DB_HOST','DB_LOGIN','DB_PWD','DB_NAME',DB_PORT);
+$mysqli = @mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
 
 if(mysqli_connect_error($mysqli)){
     die("Numéro d'erreur: ".mysqli_connect_errno($mysqli)."<br>Affichage de l'erreur: ".utf8_encode(mysqli_connect_error($mysqli)));
 }
 
-mysqli_set_charset($mysqli,'DB_CHARSET');
+mysqli_set_charset($mysqli,DB_CHARSET);
