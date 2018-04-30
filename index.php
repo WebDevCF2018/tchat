c7ba2e2ca6a7d303df01e98be0a28357bf87b404
michaeljpitz <michaeljpitz@gmail.com>
2018-04-30 11:20:28 +0200

    index + controller

--- index.php
+++ index.php
@@ -2,18 +2,7 @@
 	/* Connexion à la base de donnée */
 	require_once "config.php";
 	require_once "mysqliConnect.php";
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Chat WebDev2018</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-	<?php
+
 		/* Chargement de controller principal */
 		require "controller/mainController.php";
 	?>
-</body>
-</html>
