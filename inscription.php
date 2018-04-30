7db70e4e448899c7054668f3b31c1dffb84a8ffb
Dimitriweb92 <dimitri.moyson@cf2m.lan>
2018-04-30 13:09:27 +0200

    inscription test function newuser ()

--- inscription.php
+++ inscription.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+    include_once "mysqliConnect.php";
+    include_once "function.php";
+
+
+
+    if (isset($_POST['thelogin']) && isset($_POST["thepwd"])&& isset($_POST["themail"])){
+
+        $login= htmlspecialchars(strip_tags(trim($_POST['thelogin'])),ENT_QUOTES);
+        $pwd= strip_tags(trim($_POST['thepwd']));
+        $email= filter_var($_POST['themail'], FILTER_VALIDATE_EMAIL);
+
+    }else{
+        return "problème survenue lors de l'envoye !";
+    }
+    $envReq = newuser($mysqli,$login,$pwd);
+    ?>
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" href="css/inscription_style.css">
@@ -14,7 +31,7 @@
     </div>
     <div>
         <label for="pwd">Password :</label>
-        <input type="password" id="thepwd" name="">
+        <input type="password" id="pwd" name="thepwd">
 
     </div>
     <div>
@@ -22,7 +39,9 @@
         <input type="email" id="mail" name="themail">
     </div>
     <div>
-        <input id="button" type="submit" name="thesend">
+        <input id="button" type="submit" name="thesend" onclick="<?=$envReq;?>">
+
+
     </div>
 </form>
 
