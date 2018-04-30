2cdf630d1e3981d3f71d50f544700c4cdde0aa4d
signupromain <romain@machaharson.com>
2018-04-30 13:25:25 +0200

    css+connect.php update

--- inscription.php
+++ inscription.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" href="css/inscription_style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <form action="" method="post">
