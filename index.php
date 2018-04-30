3b11143e999dd31dbfc7c1cf97ef1c90db949d1d
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-04-30 13:38:10 +0200

    add require function

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	/* Connexion à la base de donnée */
 	require_once "config.php";
 	require_once "mysqliConnect.php";
+	require_once "function.php";
 
 		/* Chargement de controller principal */
 		require "controller/mainController.php";
