242c47dbdd1118d7c6b67ce282841c51389cea63
Ichtuus <nicolasabdoul@outlook.fr>
2018-05-04 10:09:26 +0200

    ajout du fichier validate.php qui appel les variable GET id et key

---
+++ validate.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Validation</title>
+    </head>
+
+<body>
+
+<?php
+
+if(isset($_GET['id'])&& isset($_GET['key'])){
+    $clef=$_GET['key'];
+    $identifiant=$_GET['id'];
+    echo "Votre compte à été validé !";
+}else{
+
+    false;
+    
+}
+
+?>
+
+</body>
+</html>
+
+
