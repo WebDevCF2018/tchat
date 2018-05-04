801453128bdc1c5070203b1df4465f77e04dd2b0
Ichtuus <nicolasabdoul@outlook.fr>
2018-05-04 10:37:30 +0200

    actualisation

--- connect.php
+++ connect.php
@@ -6,15 +6,16 @@
 </head>
 <body>
 <form action="" method="post">
-    <h1>Connexion</h1>
-        <label for="login">Login :</label>
-        <input type="text" id="login" name="thelogin">
-        <label for="thepwd">Password :</label>
-        <input type="password" id="thepwd" name="">
-        <input id="button" type="submit" name="thesend">
 
-        <div class="inscription">
-            <a href="?p=inscription">Inscription</a>
-        </div>
+    <h1>Connexion</h1>
+    <label for="login">Login :</label>
+    <input type="text" id="login" name="thelogin">
+    <label for="thepwd">Password :</label>
+    <input type="password" id="thepwd" name="">
+    <input id="button" type="submit" name="thesend">
+
+    <div class="inscription">
+        <a href="?p=inscription">Inscription</a>
+    </div>
 </body>
 </html>
