0766a07d3fc31e3b7e34b52b95ff8a56d0494ad1
Michael J. Pitz <michaeljpitz@gmail.com>
2018-05-04 09:59:36 +0200

    pas bon endroit
    pas bon endroit

--- controller/mainController.php
+++ controller/mainController.php
@@ -20,28 +20,3 @@
 	}else{
 		require "connect.php";
 	}
-
-
-$connect = connectUser($mysqli,$_POST['theLogin'],$_POST['thePass']);
-// on s'est connecté
-if($connect){
-    /*
-    $_SESSION['idutil'] = $connect['iduser']; // iduser
-    $_SESSION['login'] = $connect['login'];
-    $_SESSION['name'] = $connect['username'];
-    $_SESSION['permissionname'] = $connect['permissionname'];
-    plus simple:
-    */
-    // on met toutes les variables récupérées depuis la db directement dans la session
-    $_SESSION = $connect;
-    // on ajoute à la variable super globale de session 'myKey' qui stoque la clef de session (PHPSESSID)
-    $_SESSION['myKey'] = session_id(); // id de session
-
-    // on réactulise sur l'accueil pour passer en mode "admin"
-    header("Location: ./");
-
-}else{
-    $erreur_login = "Login et/ou mot de passe incorrect";
-    require_once "v/login.html.php";
-}
-?>
