96b8478da8039f8d0327db63ca1353a0783b1821
Shaban Kaloshi <skaloshi@gmail.com>
2018-05-07 10:32:56 +0200

    Ajoute de la page deco.php

--- deco.php
+++ deco.php
@@ -6,6 +6,16 @@ if(!isset($_SESSION["key"])){
 }
 
 $nom = $_SESSION["thelogin"];
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 session_destroy();
 echo '<meta http-equiv="refresh" content="3">';
 ?>
