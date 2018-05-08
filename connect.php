3502cd607d3413987fcea3f7b994738b2f80ee02
Shaban Kaloshi <skaloshi@gmail.com>
2018-05-08 10:08:30 +0200

    Modification CSS tchat.php

--- connect.php
+++ connect.php
@@ -44,6 +44,7 @@
     <meta charset="UTF-8">
     <title>connect</title>
     <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <?php if (isset($erreur)) {?>
