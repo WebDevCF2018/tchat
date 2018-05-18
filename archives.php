b9af6649f593c1dc496880a27a6fbf3ccf67caf1
luckybxl <luccia.g@gmail.com>
2018-05-18 13:31:15 +0200

    pagination ok+css

--- archives.php
+++ archives.php
@@ -2,11 +2,37 @@
 require "verifSession.php";
 require_once "mysqliConnect.php";
 require_once "function.php";
-$sql = "SELECT m.*,u.thelogin,u.thecolor FROM themessage m 
+
+$nb_par_page = 10;
+if(!isset($_GET['idarticle'])) {
+// pour pagination
+    if (isset($_GET['pg']) && ctype_digit($_GET['pg'])) {
+        $pg = (int)$_GET['pg'];
+    } else {
+        $pg = 1;
+    }
+}
+/*/*
+ *  calcul pour la pagination
+ */
+// nombre total d'article
+$requete = mysqli_query($mysqli, "SELECT COUNT(idmessage) AS nb FROM themessage;");
+$requete_assoc = mysqli_fetch_assoc($requete);
+$nb_tot = $requete_assoc['nb'];
+// calcul pour le premier argument du LIMIT
+$limit = ($pg-1)*$nb_par_page;
+// requête pour récupérer tous les articles suivant la pagination
+
+$sql = "SELECT m.*,u.thelogin,u.thecolor 
+        FROM themessage m 
         INNER JOIN theuser u 
-          ON u.idutil = m.theuser_idutil
-ORDER BY m.idmessage DESC";
+        ON u.idutil = m.theuser_idutil
+        ORDER BY m.idmessage DESC 
+        LIMIT $limit, $nb_par_page";
 $recup = mysqli_query($mysqli,$sql) or die(mysqli_error($mysqli));
+$pagination = maPagination($nb_tot, $pg,"pg",$nb_par_page);
+
+
 // pas de résultats
 if(!mysqli_num_rows($recup)){
     echo "<h3>No message yet !</h3>";
@@ -24,6 +50,7 @@ if(!mysqli_num_rows($recup)){
         <script src="js/ajax.min.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link rel="stylesheet" media="screen" href="css/style.css">
+
     </head>
 
     <body class="tchat" >
@@ -52,5 +79,6 @@ if(!mysqli_num_rows($recup)){
 			}
 			?>
     </div>
+        <p><?=$pagination?></p>
     </body>
 </html>
