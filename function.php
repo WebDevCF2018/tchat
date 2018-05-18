836d479300cc6503ecb843b2302bae9fc310a2fd
signupromain <romain@machaharson.com>
2018-05-18 14:06:11 +0200

    external links

--- function.php
+++ function.php
@@ -243,3 +243,23 @@ function updateUser($db,$idutil,$password,$repassword){
         }
     }
 }
+
+/* Fonctions de Romain */
+
+/*  liens cliquables qui s'ouvrent dans une nouvelle fenêtre */
+
+function links($text){
+
+    $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+
+    if(preg_match($reg_exUrl, $text, $url)) {
+
+        // make the urls hyper links
+        return preg_replace($reg_exUrl, '<a href="'.$url[0].'" rel="nofollow" target="_blank">'.$url[0].'</a>', $text);
+
+    } else {
+
+        // if no urls in the text just return the text
+        return $text;
+    }
+}
