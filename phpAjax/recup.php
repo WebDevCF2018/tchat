dbc75fd0460750f51f8fd36c6aba4817aa242172
kevinkin01 <kevinkinanga41@gmail.com>
2018-05-25 15:12:34 +0200

    online3

--- phpAjax/recup.php
+++ phpAjax/recup.php
@@ -31,11 +31,11 @@ if (!mysqli_num_rows($recup)) {
                 <?php
                 if(in_array($item['idutil'],$_SESSION['online'])) {
                     ?>
-                    <img src="../img/online.jpg " width="10px" height="10px">
+                    <img src="../img/green.png " >
                     <?php
                 }else {
                     ?>
-                    <img src="../img/offline.jpg" width="10px" height="10px">
+                    <img src="../img/red.png" >
                     <?php
                 }
                     ?>
